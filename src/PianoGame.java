4551f399c5817b160fd283e0d3683216164bf426
gheun <gheun098@gmail.com>
2024-11-29 10:06:18 +0900

    ✨[feat] 게임 플레이 기능 추가

--- src/PianoGame.java
+++ src/PianoGame.java
@@ -3,21 +3,20 @@ package src;
 import javax.swing.*;
 import java.awt.*;
 
-public class PianoGame extends JFrame
-{
+public class PianoGame extends JFrame {
+    private String musicTitle;
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new PianoGame().setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            Music defaultMusic = new Music("Default Song", "sound/sing/WelcomToTheShow", "easy");
+            new PianoGame(defaultMusic).setVisible(true);
         });
     }
 
-    private JPanel sheetMusicPanel;
+    // Music 객체를 매개변수로 받는 생성자
+    public PianoGame(Music music) {
+        this.musicTitle = music.getTitle();
 
-    public PianoGame() {
-        setTitle("Sound Mate - Game_Piano");         // super("Sound Mate"); 같은 기능
+        setTitle("PianoGame - " + musicTitle);
         this.setSize(1280, 720);
         this.getContentPane().setBackground(Color.white);
         this.setLocationRelativeTo(null);       // 창을 가운데에 띄움
@@ -25,19 +24,27 @@ public class PianoGame extends JFrame
         this.setVisible(true);
         this.setLayout(new BorderLayout());
 
-        // 컨트롤러 패널
-        src.gameBar gameBar = new gameBar();
-        this.add(gameBar, BorderLayout.NORTH);
 
-        // 피아노 키보드 패널
+//        // 곡 정보를 표시
+//        JLabel songLabel = new JLabel("Now Playing: " + musicTitle + SwingConstants.CENTER);
+//        songLabel.setFont(new Font("Arial", Font.BOLD, 24));
+//        add(songLabel, BorderLayout.NORTH);
+
+        // 노트 트랙 패널
+        Track track = new Track();
+        add(track, BorderLayout.CENTER);
+
+        // 상단 게임바 패널
+        gameBar gameBar = new gameBar();
+        add(gameBar, BorderLayout.NORTH);
+
+        // 하단 피아노 키보드 패널
         Piano piano = new Piano();
-        //PianoInterface pianoInterface = new PianoInterface(controlInterface);
-        this.add(piano, BorderLayout.SOUTH);
+        add(piano, BorderLayout.SOUTH);
 
-        // 키보드 포커스를 PianoInterface에 설정
+        // 키보드 포커스 설정
         piano.requestFocusInWindow();
 
-
+        setVisible(true);
     }
-
 }
